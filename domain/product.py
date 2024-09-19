57bf3025c8d8ee8d4ce1298a72d3004c3ed20bf8
Fernando Salom <fer@rudo.es>
2024-09-19 17:33:46 +0200

    Add product

--- domain/product.py
+++ domain/product.py
@@ -1,5 +1,6 @@
 class Product:
-    def __init__(self, name: str, category: str, price: float):
+    def __init__(self, name: str, quantity: int, price_per_unit: float, price: float):
         self.name = name
-        self.category = category
+        self.quantity = quantity
+        self.price_per_unit = price_per_unit
         self.price = price
