0e85e22a2d5c99b5089970699c1c7f609196af79
Fernando Salom <fer@rudo.es>
2024-09-21 08:54:49 +0200

    Fix ticket domain

--- domain/ticket.py
+++ domain/ticket.py
@@ -2,6 +2,9 @@ from domain.product import Product
 
 
 class Ticket:
-    def __init__(self, products: [Product], total: float):
-        self.products = [Product]
+    def __init__(self, products: [Product], total: float, date: str, email: str, location: str):
+        self.products = products
         self.total = total
+        self.date = date
+        self.email = email
+        self.location = location
