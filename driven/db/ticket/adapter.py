d48c18c7230e04d6fffec799f686c5b63e32f2d7
Fernando Salom <fer@rudo.es>
2024-09-22 14:09:20 +0200

    Add ticket adapter

--- driven/db/ticket/adapter.py
+++ driven/db/ticket/adapter.py
@@ -1,10 +1,25 @@
 from application.ports.driven.database.ticket.db_repository import TicketDBRepositoryPort
+from domain.ticket import Ticket
+from driven.db.product.models import ProductDBO
+from driven.db.ticket.models import TicketDBO
 
 
 class TicketDBRepositoryAdapter(TicketDBRepositoryPort):
     def __init__(self):
         print("")
 
-    def save(self, mail: str):
-        pass
+    def save(self, ticket: Ticket):
+        productsDBO = []
+        for product in ticket.products:
+            productsDBO.append(
+                ProductDBO.objects.get_or_create(
 
+                )
+            )
+
+        ticket_dbo = TicketDBO.objects.create(
+            date=ticket.date,
+            location=ticket.location,
+            email=ticket.email,
+            product=productsDBO
+        )
