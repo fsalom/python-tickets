6137dd16c7f9ce93300023bedb3d058887c3a20f
Fernando Salom <fer@rudo.es>
2024-09-25 17:10:48 +0200

    Fix domain entities

--- domain/product.py
+++ domain/product.py
@@ -1,6 +1,7 @@
 class Product:
-    def __init__(self, name: str, quantity: int, price_per_unit: float, price: float):
+    def __init__(self, name: str, quantity: int, price_per_unit: float, price: float, weight: str):
         self.name = name
         self.quantity = quantity
         self.price_per_unit = price_per_unit
         self.price = price
+        self.weight = weight
--- domain/ticket.py
+++ domain/ticket.py
@@ -2,9 +2,17 @@ from domain.product import Product
 
 
 class Ticket:
-    def __init__(self, products: [Product], total: float, date: str, email: str, location: str):
+    def __init__(self, id_ticket: str,
+                 products: [Product],
+                 total: float,
+                 iva: float,
+                 date: str,
+                 email: str,
+                 location: str):
+        self.id = id_ticket
         self.products = products
         self.total = total
         self.date = date
         self.email = email
         self.location = location
+        self.iva = iva
