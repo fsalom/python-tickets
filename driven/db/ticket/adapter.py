5d2a9e7132c536c27076244be02557b773b8e6c0
Fernando Salom <fer@rudo.es>
2024-09-26 09:09:03 +0200

    Add initial db adapter for tickets

--- driven/db/ticket/adapter.py
+++ driven/db/ticket/adapter.py
@@ -1,25 +1,58 @@
 from application.ports.driven.database.ticket.db_repository import TicketDBRepositoryPort
 from domain.ticket import Ticket
-from driven.db.product.models import ProductDBO
-from driven.db.ticket.models import TicketDBO
+from driven.db.product.models import ProductDBO, ProductPriceHistoryDBO
+from driven.db.store.models import StoreDBO
+from driven.db.ticket.models import TicketDBO, TicketProductDBO
+from driven.db.user.models import UserDBO
 
 
 class TicketDBRepositoryAdapter(TicketDBRepositoryPort):
     def __init__(self):
-        print("")
+        pass
 
     def save(self, ticket: Ticket):
-        productsDBO = []
-        for product in ticket.products:
-            productsDBO.append(
-                ProductDBO.objects.get_or_create(
+        products_dbo = []
 
-                )
+        # Crear la tienda
+        store_dbo = StoreDBO.objects.create(
+            location_info=ticket.location
+        )
+
+        user_dbo, created = UserDBO.objects.get_or_create(
+            email=ticket.email
+        )
+
+        # Crear el ticket
+        ticket_dbo = TicketDBO.objects.create(
+            date_raw=ticket.date,
+            location=store_dbo,
+            email=user_dbo,
+            total=ticket.total
+        )
+
+        for product in ticket.products:
+            product_dbo, created = ProductDBO.objects.get_or_create(
+                name=product.name
             )
 
-        ticket_dbo = TicketDBO.objects.create(
-            date=ticket.date,
-            location=ticket.location,
-            email=ticket.email,
-            product=productsDBO
-        )
+            product_history_price = ProductPriceHistoryDBO.objects.create(
+                product=product_dbo,
+                date_raw=ticket.date,
+                price=product.price,
+                price_per_unit=product.price_per_unit
+            )
+
+            TicketProductDBO.objects.create(
+                ticket=ticket_dbo,
+                product=product_dbo,
+                quantity=product.quantity,
+                history_price=product_history_price,
+                units=product.price_per_unit
+            )
+
+            # Añadir product_dbo a la lista en lugar de ticket_product_dbo
+            products_dbo.append(product_dbo)
+
+        # Asignar la lista de productos (ProductDBO) a la relación ManyToMany
+        ticket_dbo.products.set(products_dbo)
+
