74ee0173388f802cc0b03ee6e3918d3fd24e877d
Fernando Salom <fer@rudo.es>
2024-09-27 18:31:50 +0200

    Add mail entity

--- domain/mail.py
+++ domain/mail.py
@@ -2,7 +2,8 @@ from domain.ticket import Ticket
 
 
 class Mail:
-    def __init__(self, mail: str, subject: str, content: str):
-        self.mail = mail
+    def __init__(self, email: str, subject: str, content: str, date_raw: str):
+        self.email = email
+        self.date_raw = date_raw
         self.subject = subject
         self.content = content
