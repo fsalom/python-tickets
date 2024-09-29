d8d1ea4288197f3ee4c9d0a67fb7a682a361f125
Fernando Salom <fer@rudo.es>
2024-09-29 19:19:47 +0200

    Add security utility

---
+++ driving/api_rest/security/security.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+from asgiref.sync import sync_to_async
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+from application.di.authentication.injector import AuthenticationInjector
+from application.services.authentication_services import AuthenticationServices
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_user_or_refuse(token: Annotated[str, Depends(oauth2_scheme)],
+                             authentication_service: AuthenticationServices
+                             = Depends(AuthenticationInjector().get_service)):
+    user = await sync_to_async(authentication_service.get_user)(token)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+        )
+
+    return user
