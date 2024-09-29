d89038bcac4aac2e46663b9e2d31e637ee2dfebb
Fernando Salom <fer@rudo.es>
2024-09-29 19:25:21 +0200

    Add router with endpoints

---
+++ driving/api_rest/router/router.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from driving.api_rest.v1.authentication.adapter import auth_router
+from driving.api_rest.v1.tickets.adapter import ticket_router
+from driving.api_rest.v1.user.adapter import user_router
+
+
+def add_routers(app: FastAPI):
+    # v1
+    app.include_router(auth_router, prefix='/v1')
+    app.include_router(user_router, prefix='/v1')
+    app.include_router(ticket_router, prefix='/v1')
