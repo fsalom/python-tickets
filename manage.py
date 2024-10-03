3a3c9793de50cce26affa0d57c8751b39fd7347b
Fernando Salom <fer@rudo.es>
2024-10-03 21:43:46 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,22 +6,27 @@ import threading
 import time
 import django
 
+
 def cronjob():
-    # Forzar la escritura en consola sin buffering
     sys.stdout.write("Starting cronjob\n")
     sys.stdout.flush()
 
-    # Importar módulos dependientes de Django después de la inicialización
-    from application.services.mail_processor_services import MailProcessorServices
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infrastructure.django.settings')
+    django.setup()  # Asegura que las apps están listas
+
+    from application.services.mail_services import MailServices
     from driven.db.ticket.adapter import TicketDBRepositoryAdapter
     from driven.mail.mail_repository_adapter import MailRepositoryAdapter
     from driving.mails.adapter import MailsAdapter
+    from driven.db.mail.adapter import MailDBRepositoryAdapter
 
     mail_repository_adapter = MailRepositoryAdapter()
     ticket_db_repository_adapter = TicketDBRepositoryAdapter()
+    mail_db_repository_adapter = MailDBRepositoryAdapter()
 
-    service = MailProcessorServices(mail_repository=mail_repository_adapter,
-                                    ticket_db_repository=ticket_db_repository_adapter)
+    service = MailServices(mail_repository=mail_repository_adapter,
+                           ticket_db_repository=ticket_db_repository_adapter,
+                           mail_db_repository=mail_db_repository_adapter)
 
     adapter = MailsAdapter(service=service)
     adapter.schedule_jobs()
@@ -35,7 +40,7 @@ def cronjob():
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boot.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infrastructure.django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -45,12 +50,30 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     # Iniciar el cronjob en un hilo separado
-    #cron_thread = threading.Thread(target=cronjob)
-    #cron_thread.daemon = True  # Esto asegura que el hilo se detenga cuando el servidor se detenga
-    #cron_thread.start()
-
+    cron_thread = threading.Thread(target=cronjob)
+    cron_thread.daemon = True  # Esto asegura que el hilo se detenga cuando el servidor se detenga
+    cron_thread.start()
+    test()
     # Continuar con el arranque normal de Django
     execute_from_command_line(sys.argv)
+
+
+def test():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infrastructure.django.settings')
+    django.setup()  # Asegura que las apps están listas
+    from application.services.mail_services import MailServices
+    from driven.db.ticket.adapter import TicketDBRepositoryAdapter
+    from driven.mail.mail_repository_adapter import MailRepositoryAdapter
+    from driven.db.mail.adapter import MailDBRepositoryAdapter
+
+    mail_repository_adapter = MailRepositoryAdapter()
+    ticket_db_repository_adapter = TicketDBRepositoryAdapter()
+    mail_db_repository_adapter = MailDBRepositoryAdapter()
+
+    service = MailServices(mail_repository=mail_repository_adapter,
+                           ticket_db_repository=ticket_db_repository_adapter,
+                           mail_db_repository=mail_db_repository_adapter)
+    service.test()
 
 
 if __name__ == '__main__':
