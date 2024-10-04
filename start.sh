78dda32974b1a2102b9fe60009e241cf0e09da99
Fernando Salom <fer@rudo.es>
2024-10-04 22:03:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ echo "Ejecutando collectstatic de Django..."
 python manage.py collectstatic --noinput
 
 echo "Iniciando el servidor de Django en segundo plano..."
-python manage.py runserver 0.0.0.0:8000 &
+python manage.py runserver 0.0.0.0:8000 
 
-echo "Iniciando Gunicorn con FastAPI en segundo plano..."
-gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001 &
+#echo "Iniciando Gunicorn con FastAPI en segundo plano..."
+#gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001 &
 
 # Mantener el script en ejecución
 wait
