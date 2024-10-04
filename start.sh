ab7826be24c8b1cf411d61046987bc7ba4cdafa8
Fernando Salom <fer@rudo.es>
2024-10-04 22:48:45 +0200

    Update deploy

--- start.sh
+++ start.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-echo "Ejecutando migraciones de Django..."
-python manage.py migrate
-
-echo "Ejecutando collectstatic de Django..."
-python manage.py collectstatic --noinput
-
-echo "Iniciando el servidor de Django en segundo plano..."
-python manage.py runserver 0.0.0.0:8000
 
 #echo "Iniciando Gunicorn con FastAPI en segundo plano..."
-#gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001 &
+gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001 &
 
 # Mantener el script en ejecución
 wait
