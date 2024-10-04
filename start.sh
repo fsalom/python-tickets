833ad35b4f12768354085b6dfc86918f53612169
Fernando Salom <fer@rudo.es>
2024-10-04 23:27:57 +0200

    Add dockerignore

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-#echo "Iniciando Gunicorn con FastAPI en segundo plano..."
+echo "Ejecutando migraciones de Django..."
+python manage.py migrate
+
+echo "Ejecutando collectstatic de Django..."
+python manage.py collectstatic --noinput
+
+echo "Iniciando el servidor de Django en segundo plano..."
+python manage.py runserver 0.0.0.0:8000 &
+
+echo "Iniciando Gunicorn con FastAPI en segundo plano..."
 gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001 &
 
 # Mantener el script en ejecución
