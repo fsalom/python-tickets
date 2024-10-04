90534deddd11c9f6f1bd0f875f63d10cface9060
Fernando Salom <fer@rudo.es>
2024-10-04 21:05:20 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+Copy code
+#!/bin/bash
+
 # Ejecutar migraciones de Django
 echo "Ejecutando migraciones de Django..."
 python manage.py migrate
 
-echo "Ejecutando servidor"
-python manage.py runserver
+# Iniciar el servidor de Django en segundo plano
+echo "Iniciando el servidor de Django en segundo plano..."
+python manage.py runserver 0.0.0.0:8000 &
 
-# Iniciar Gunicorn con FastAPI
-echo "Iniciando Gunicorn con FastAPI..."
-gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+# Iniciar Gunicorn con FastAPI en segundo plano
+echo "Iniciando Gunicorn con FastAPI en segundo plano..."
+gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001 &
 
-# Alternativa para levantar Django, si se prefiere en algún caso específico:
-# echo "Levantando Django..."
-# python manage.py runserver 0.0.0.0:8000
+# Mantener el script en ejecución
+wait
