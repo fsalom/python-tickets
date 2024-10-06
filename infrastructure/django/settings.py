871080c96e7d74885421ef3ea250c22a1322ed0a
Fernando Salom <fer@rudo.es>
2024-10-06 09:56:20 +0200

    Update settings.py

--- infrastructure/django/settings.py
+++ infrastructure/django/settings.py
@@ -154,8 +154,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATICFILES_DIRS = [
-   '/code/static/',
+   '../static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
