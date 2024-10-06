f07c0a2bb783f1327e251c3ef3102ac89751a6e3
Fernando Salom <fer@rudo.es>
2024-10-06 10:03:57 +0200

    Update settings.py

--- infrastructure/django/settings.py
+++ infrastructure/django/settings.py
@@ -150,13 +150,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+# Donde Django recolectará todos los archivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+# Directorios donde buscar archivos estáticos adicionales
 STATICFILES_DIRS = [
-   '../static/',
+   os.path.join(BASE_DIR, 'static'),  # Tu carpeta 'static' local
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
