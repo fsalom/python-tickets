e163c1703fca78edbeed7c36678d46b1bfe1db63
Fernando Salom <fer@rudo.es>
2024-10-13 05:19:19 +0200

    Add containers DI for fastapi on start up

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 import django
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infrastructure.django.settings')
 django.setup()
 
 from fastapi import FastAPI
 from driving.api_rest.router.router import add_routers
+from driving.api_rest.containers.containers import add_containers
 
 app = FastAPI()
 add_routers(app)
+add_containers(app)
 
 
 @app.get("api/health")
