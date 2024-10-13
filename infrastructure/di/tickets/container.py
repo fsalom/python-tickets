026001f81a90e0d76a70e6d15871abae22c382eb
Fernando Salom <fer@rudo.es>
2024-10-13 05:09:46 +0200

    Add tickets container

---
+++ infrastructure/di/tickets/container.py
@@ -0,0 +1,16 @@
+from dependency_injector import containers, providers
+
+from application.services.ticket_services import TicketServices
+from driven.db.ticket.adapter import TicketDBRepositoryAdapter
+
+
+class TicketContainer(containers.DeclarativeContainer):
+    config = providers.Configuration()
+    wiring_config = containers.WiringConfiguration(modules=["driving.api_rest.v1.tickets.adapter"])
+
+    db_repository = providers.Factory(TicketDBRepositoryAdapter)
+
+    service = providers.Factory(
+        TicketServices,
+        db_repository=db_repository
+    )
