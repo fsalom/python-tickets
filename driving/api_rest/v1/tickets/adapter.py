8cba18cb936d536388cbdd2998abe76c4bab704d
Fernando Salom <fer@rudo.es>
2024-10-13 05:14:36 +0200

    Change injector by container DI for tickets endpoints

--- driving/api_rest/v1/tickets/adapter.py
+++ driving/api_rest/v1/tickets/adapter.py
@@ -1,7 +1,10 @@
 from typing import Annotated
 
+from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
+from application.ports.driving.tickets_service_port import TicketServicePort
+from infrastructure.di.tickets.container import TicketContainer
 from infrastructure.di.tickets.injector import TicketInjector
 from application.services.ticket_services import TicketServices
 from domain.user import User
@@ -11,13 +14,15 @@ ticket_router = APIRouter()
 
 
 @ticket_router.get('/tickets/all')
+@inject
 async def me(user: Annotated[User, Depends(get_user_or_refuse)],
-             service: TicketServices = Depends(TicketInjector().get_service)):
+             service: TicketServicePort = Depends(Provide[TicketContainer.service])):
     tickets = await service.get_ticket_for_user(user=user.email)
     return {"num_tickets": len(tickets), "tickets": [ticket.__dict__ for ticket in tickets]}
 
 
 @ticket_router.get('/tickets/test')
-async def me(service: TicketServices = Depends(TicketInjector().get_service)):
+@inject
+async def me(service: TicketServices = Depends(Provide[TicketContainer.service])):
     tickets = await service.get_ticket_for_user(user="fdosalom@gmail.com")
     return {"num_tickets": len(tickets), "tickets": [ticket.__dict__ for ticket in tickets]}
