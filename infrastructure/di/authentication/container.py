cfec487d59987e191aaf591a21b690e2db488b86
Fernando Salom <fer@rudo.es>
2024-10-14 08:19:09 +0200

    Replace singleton with factory

--- infrastructure/di/authentication/container.py
+++ infrastructure/di/authentication/container.py
@@ -1,4 +1,5 @@
 from dependency_injector import containers, providers
+from fastapi.security import OAuth2PasswordBearer
 
 from application.services.authentication_services import AuthenticationServices
 from driven.db.authentication.adapter import AuthenticationDBRepositoryAdapter
@@ -8,7 +9,8 @@ class AuthenticationContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
     wiring_config = containers.WiringConfiguration(modules=["driving.api_rest.security.security",
                                                             "driving.api_rest.v1.authentication.adapter"])
-    db_repository = providers.Singleton(AuthenticationDBRepositoryAdapter)
+    db_repository = providers.Factory(AuthenticationDBRepositoryAdapter)
+    oauth2_scheme = providers.Singleton(OAuth2PasswordBearer, tokenUrl="token")
 
     service = providers.Factory(
         AuthenticationServices,
