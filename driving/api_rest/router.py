f87603bce75169bcd68b200dbcbcc7cc094b27a9
Fernando Salom <fer@rudo.es>
2024-10-14 08:53:51 +0200

    Change location of support files for api_rest and remove unnecessary files

--- driving/api_rest/router.py
+++ driving/api_rest/router.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
+from driving.api_rest.health.adapter import health_router
 from driving.api_rest.v1.authentication.adapter import auth_router
 from driving.api_rest.v1.tickets.adapter import ticket_router
 from driving.api_rest.v1.user.adapter import user_router
 
 
 def add_routers(app: FastAPI):
+    app.include_router(health_router, prefix='/api')
     # v1
     app.include_router(auth_router, prefix='/api/v1')
     app.include_router(user_router, prefix='/api/v1')
