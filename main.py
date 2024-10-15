b0354bba4d5a394fc79eb0fd60d6d664a4fdeb06
Fernando Salom <fer@rudo.es>
2024-10-15 10:05:26 +0200

    Remove health endpoint from main

--- main.py
+++ main.py
@@ -5,14 +5,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infrastructure.django.settings'
 django.setup()
 
 from fastapi import FastAPI
-from driving.api_rest.router.router import add_routers
-from driving.api_rest.containers.containers import add_containers
+from driving.api_rest.router import add_routers
+from driving.api_rest.containers import add_containers
 
 app = FastAPI()
 add_routers(app)
 add_containers(app)
-
-
-@app.get("api/health")
-async def health_check():
-    return {"status": "ok"}
