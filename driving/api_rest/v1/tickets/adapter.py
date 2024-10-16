3cba3a56683411e225ce8d1bb48fff7046e86d97
Fernando Salom <fer@rudo.es>
2024-10-16 09:23:58 +0200

    Add mapper to get all tickets endpoint

--- driving/api_rest/v1/tickets/adapter.py
+++ driving/api_rest/v1/tickets/adapter.py
@@ -4,6 +4,7 @@ from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
 from application.ports.driving.tickets_service_port import TicketServicePort
+from driving.api_rest.v1.tickets.mapper import TicketDTOMapper
 from infrastructure.di.tickets.container import TicketContainer
 from application.services.ticket_services import TicketServices
 from domain.user import User
@@ -15,9 +16,10 @@ ticket_router = APIRouter()
 @ticket_router.get('/tickets/all')
 @inject
 async def me(user: Annotated[User, Depends(get_user_or_refuse)],
-             service: TicketServicePort = Depends(Provide[TicketContainer.service])):
+             service: TicketServicePort = Depends(Provide[TicketContainer.service]),
+             mapper: TicketDTOMapper = Depends(Provide[TicketContainer.mapper])):
     tickets = await service.get_ticket_for_user(user=user.email)
-    return {"num_tickets": len(tickets), "tickets": [ticket.__dict__ for ticket in tickets]}
+    return mapper.to_dto(tickets)
 
 
 @ticket_router.get('/tickets/test')
