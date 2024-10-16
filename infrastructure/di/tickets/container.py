22bed6d32b1c83df369f835b1daf0504a4d5edb0
Fernando Salom <fer@rudo.es>
2024-10-16 09:22:47 +0200

    Remove injector and fix container

--- infrastructure/di/tickets/container.py
+++ infrastructure/di/tickets/container.py
@@ -2,6 +2,7 @@ from dependency_injector import containers, providers
 
 from application.services.ticket_services import TicketServices
 from driven.db.ticket.adapter import TicketDBRepositoryAdapter
+from driving.api_rest.v1.tickets.mapper import TicketDTOMapper
 
 
 class TicketContainer(containers.DeclarativeContainer):
@@ -13,4 +14,8 @@ class TicketContainer(containers.DeclarativeContainer):
     service = providers.Factory(
         TicketServices,
         db_repository=db_repository
+    )
+
+    mapper = providers.Factory(
+        TicketDTOMapper
     )
