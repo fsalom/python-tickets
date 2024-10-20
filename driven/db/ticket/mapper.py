378b7709dfe9f93372da88d12f2144fb50f38ed6
Fernando Salom <fer@rudo.es>
2024-10-20 15:13:41 +0200

    Fix mapper for DB

--- driven/db/ticket/mapper.py
+++ driven/db/ticket/mapper.py
@@ -1,6 +1,9 @@
 from domain.product import Product
 from domain.ticket import Ticket
+from driven.db.product.models import ProductDBO, ProductPriceHistoryDBO
+from driven.db.store.models import StoreDBO
 from driven.db.ticket.models import TicketDBO
+from driven.db.user.models import UserDBO
 
 
 class TicketDBMapper:
@@ -31,3 +34,42 @@ class TicketDBMapper:
                 )
             )
         return tickets
+
+    @staticmethod
+    def map_store(ticket: Ticket) -> StoreDBO:
+        store_dbo, _ = StoreDBO.objects.get_or_create(
+            location_info=ticket.location
+        )
+        return store_dbo
+
+    @staticmethod
+    def map_user(ticket: Ticket) -> UserDBO:
+        user_dbo, _ = UserDBO.objects.get_or_create(
+            email=ticket.email,
+            username=ticket.email
+        )
+        return user_dbo
+
+    @staticmethod
+    def map_product(product: Product, date: str) -> (ProductDBO, ProductPriceHistoryDBO):
+        product_dbo, _ = ProductDBO.objects.get_or_create(
+            name=product.name
+        )
+        product_history_price, _ = ProductPriceHistoryDBO.objects.get_or_create(
+            product=product_dbo,
+            date_raw=date,
+            price=product.price,
+            price_per_unit=product.price_per_unit
+        )
+        return product_dbo, product_history_price
+
+    @staticmethod
+    def map_ticket(ticket: Ticket, store_dbo: StoreDBO, user_dbo: UserDBO) -> TicketDBO:
+        ticket_dbo, _ = TicketDBO.objects.get_or_create(
+            date_raw=ticket.date,
+            location=store_dbo,
+            email=user_dbo,
+            total=ticket.total,
+            id_ticket=ticket.id
+        )
+        return ticket_dbo
