3828c565dde412ef90d5d842d9bdc3f95593f50b
Fernando Salom <fer@rudo.es>
2024-10-21 12:49:02 +0200

    Add database repository port for tickets

--- application/ports/driven/database/ticket/db_repository.py
+++ application/ports/driven/database/ticket/db_repository.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+from domain.product import Product
 from domain.ticket import Ticket
 
 
@@ -10,4 +11,20 @@ class TicketDBRepositoryPort(ABC):
 
     @abstractmethod
     async def get_tickets_for(self, user: str) -> [Ticket]:
+        pass
+
+    @abstractmethod
+    async def get_total_for(self, user: str, start_date: str, end_date: str) -> float:
+        pass
+
+    @abstractmethod
+    async def get_number_of_tickets_for(self, user: str, start_date: str, end_date: str) -> int:
+        pass
+
+    @abstractmethod
+    async def get_number_of_products_for(self, user: str, start_date: str, end_date: str) -> int:
+        pass
+
+    @abstractmethod
+    async def get_top_products_for(self, user: str, start_date: str, end_date: str, number: int = 10) -> [Product]:
         pass
