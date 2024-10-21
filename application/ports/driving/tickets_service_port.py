181355c75421fa703ea8495abd496089da98d7e7
Fernando Salom <fer@rudo.es>
2024-10-21 12:49:37 +0200

    Add service port and implementation for tickets

--- application/ports/driving/tickets_service_port.py
+++ application/ports/driving/tickets_service_port.py
@@ -1,9 +1,26 @@
 from abc import ABC, abstractmethod
 
+from domain.product import Product
 from domain.ticket import Ticket
 
 
 class TicketServicePort(ABC):
     @abstractmethod
     async def get_ticket_for_user(self, user: str) -> [Ticket]:
+        pass
+
+    @abstractmethod
+    async def get_total_for(self, user: str, start_date: str, end_date: str) -> float:
+        pass
+
+    @abstractmethod
+    async def get_number_of_tickets_for(self, user: str, start_date: str, end_date: str) -> int:
+        pass
+
+    @abstractmethod
+    async def get_number_of_products_for(self, user: str, start_date: str, end_date: str) -> int:
+        pass
+
+    @abstractmethod
+    async def get_top_products_for(self, user: str, start_date: str, end_date: str, number: int = 10) -> [Product]:
         pass
--- application/services/ticket_services.py
+++ application/services/ticket_services.py
@@ -2,6 +2,7 @@ from asgiref.sync import sync_to_async
 
 from application.ports.driven.database.ticket.db_repository import TicketDBRepositoryPort
 from application.ports.driving.tickets_service_port import TicketServicePort
+from domain.product import Product
 from domain.ticket import Ticket
 
 
@@ -12,3 +13,15 @@ class TicketServices(TicketServicePort):
 
     async def get_ticket_for_user(self, user: str) -> [Ticket]:
         return await self.db_repository.get_tickets_for(user)
+
+    async def get_number_of_products_for(self, user: str, start_date: str, end_date: str) -> int:
+        return await self.db_repository.get_number_of_products_for(user, start_date, end_date)
+
+    async def get_number_of_tickets_for(self, user: str, start_date: str, end_date: str) -> int:
+        return await self.db_repository.get_number_of_tickets_for(user, start_date, end_date)
+
+    async def get_total_for(self, user: str, start_date: str, end_date: str) -> float:
+        return await self.db_repository.get_total_for(user, start_date, end_date)
+
+    async def get_top_products_for(self, user: str, start_date: str, end_date: str, number: int = 10) -> [Product]:
+        return await self.db_repository.get_total_for(user, start_date, end_date)
