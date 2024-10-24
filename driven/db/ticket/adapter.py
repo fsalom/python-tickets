ab7f4c17de4bf5c4c01be3a5025ce9c7234b4252
Fernando Salom <fer@rudo.es>
2024-10-24 09:01:16 +0200

    Add stats functionality for tickets db adapter

--- driven/db/ticket/adapter.py
+++ driven/db/ticket/adapter.py
@@ -1,4 +1,5 @@
 from asgiref.sync import sync_to_async
+from django.forms import models
 
 from application.ports.driven.database.ticket.db_repository import TicketDBRepositoryPort
 from domain.product import Product
@@ -30,13 +31,57 @@ class TicketDBRepositoryAdapter(TicketDBRepositoryPort):
         return tickets
 
     async def get_total_for(self, user: str, start_date: str, end_date: str) -> float:
-        pass
+        def _get_total(current_user, start_date, end_date) -> float:
+            tickets = TicketDBO.objects.filter(
+                email__email=current_user,
+                date_raw__gte=start_date,
+                date_raw__lte=end_date
+            )
+            return tickets.aggregate(total=models.Sum('total'))['total'] or 0.0
+
+        total = await sync_to_async(_get_total)(user, start_date, end_date)
+        return total
 
     async def get_number_of_tickets_for(self, user: str, start_date: str, end_date: str) -> int:
-        pass
+        def _get_ticket_count(current_user, start_date, end_date) -> int:
+            return TicketDBO.objects.filter(
+                email__email=current_user,
+                date_raw__gte=start_date,
+                date_raw__lte=end_date
+            ).count()
+
+        count = await sync_to_async(_get_ticket_count)(user, start_date, end_date)
+        return count
 
     async def get_number_of_products_for(self, user: str, start_date: str, end_date: str) -> int:
-        pass
+        def _get_product_count(current_user, start_date, end_date) -> int:
+            tickets = TicketDBO.objects.filter(
+                email__email=current_user,
+                date_raw__gte=start_date,
+                date_raw__lte=end_date
+            )
+            return TicketProductDBO.objects.filter(ticket__in=tickets).aggregate(
+                total_quantity=models.Sum('quantity')
+            )['total_quantity'] or 0
+
+        count = await sync_to_async(_get_product_count)(user, start_date, end_date)
+        return count
 
     async def get_top_products_for(self, user: str, start_date: str, end_date: str, number: int = 10) -> [Product]:
-        pass
+        def _get_top_products(current_user, start_date, end_date, number) -> [Product]:
+            tickets = TicketDBO.objects.filter(
+                email__email=current_user,
+                date_raw__gte=start_date,
+                date_raw__lte=end_date
+            )
+            top_products = TicketProductDBO.objects.filter(ticket__in=tickets).values(
+                'product__name'
+            ).annotate(total_quantity=models.Sum('quantity')).order_by('-total_quantity')[:number]
+
+            return [
+                Product(name=prod['product__name'], quantity=prod['total_quantity'])
+                for prod in top_products
+            ]
+
+        top_products = await sync_to_async(_get_top_products)(user, start_date, end_date, number)
+        return top_products
